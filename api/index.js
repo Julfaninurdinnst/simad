0619a1a803a7553753d0265ab897b2b600c7df20
julfaninurdin <julfani123@polgan.ac.id>
2022-05-18 22:49:13 +0700

    membuat api
    melakukan setup awal api

---
+++ api/index.js
@@ -0,0 +1,23 @@
+require('dotenv').config();
+const routes = require('./api/routes');
+const express = require('express');
+const mongoose = require('mongoose');
+const mongoString = process.env.DATABASE_URL;
+
+mongoose.connect(mongoString);
+const database = mongoose.connection;
+
+database.on('error', (error) => {
+    console.log(error)
+})
+
+database.once('connected', () => {
+    console.log('Database Connected');
+})
+const app = express();
+
+app.use(express.json());
+app.use('/api', routes);
+app.listen(9000, () => {
+    console.log(`Server Started`)
+})
